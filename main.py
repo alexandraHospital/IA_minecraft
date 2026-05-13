e09585debbb76d1986ee595ecbd44fee4c7aa5f1
alexandraHospital <hospital.alex@gmail.com>
2026-05-13 17:05:13 +0200

    update main, WIP

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 
 
+
+
 def main():
     # Input => photo
+    
+    # model = YOLO("/home/risson/IA_minecraft/output/models/train2/weights/best.pt")
+    # calls model on this image
+    # results = model(image_path)
+
     # output => minecraft structure
     
 if __name__ == "__main__":
